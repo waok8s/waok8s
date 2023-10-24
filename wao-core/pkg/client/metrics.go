42c80b96350ee750322b7e60abb949606c4e84fd
Shunsuke Ise <ise@bitmedia.co.jp>
2023-10-24 11:10:09 +0900

    migrated pkg/client from wao-scheduler

---
+++ wao-core/pkg/client/metrics.go
@@ -0,0 +1,135 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	custommetricsv1beta2 "k8s.io/metrics/pkg/apis/custom_metrics/v1beta2"
+	metricsv1beta1 "k8s.io/metrics/pkg/apis/metrics/v1beta1"
+	metricsclientv1beta1 "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
+	custommetricsclient "k8s.io/metrics/pkg/client/custom_metrics"
+
+	"github.com/waok8s/wao-core/pkg/metrics"
+)
+
+type CachedMetricsClient struct {
+	metricsclientset    metricsclientv1beta1.MetricsV1beta1Interface
+	custommetricsclient custommetricsclient.CustomMetricsClient
+
+	ttl   time.Duration
+	cache sync.Map
+}
+
+func NewCachedMetricsClient(metricsclientset metricsclientv1beta1.MetricsV1beta1Interface, custommetricsclient custommetricsclient.CustomMetricsClient, ttl time.Duration) *CachedMetricsClient {
+	return &CachedMetricsClient{
+		metricsclientset:    metricsclientset,
+		custommetricsclient: custommetricsclient,
+		ttl:                 ttl,
+	}
+}
+
+func metricsCacheKey(obj types.NamespacedName, metricType, metricName string) string {
+	return fmt.Sprintf("%s#%s#%s", obj.String(), metricType, metricName)
+}
+
+const (
+	metricTypeNode = "node"
+	metricTypePod  = "pod"
+
+	metricNameMetrics   = "metrics"
+	metricNameInletTemp = metrics.ValueInletTemperature
+	metricNameDeltaP    = metrics.ValueDeltaPressure
+)
+
+type metricsCache struct {
+	NodeMetrics   *metricsv1beta1.NodeMetrics
+	PodMetrics    *metricsv1beta1.PodMetrics
+	CustomMetrics map[string]*custommetricsv1beta2.MetricValue
+
+	ExpiredAt time.Time
+}
+
+func (c *CachedMetricsClient) get(ctx context.Context, obj types.NamespacedName, metricType string, metricName string) (*metricsCache, error) {
+
+	key := metricsCacheKey(obj, metricType, metricName)
+
+	if v, ok1 := c.cache.Load(key); ok1 {
+		if cv, ok2 := v.(*metricsCache); ok2 {
+			if cv.ExpiredAt.After(time.Now()) {
+				return cv, nil
+			}
+		}
+	}
+
+	cv := &metricsCache{
+		CustomMetrics: make(map[string]*custommetricsv1beta2.MetricValue),
+		ExpiredAt:     time.Now().Add(c.ttl),
+	}
+
+	switch metricType {
+	case metricTypeNode:
+		switch metricName {
+		case metricNameMetrics:
+			nodeMetrics, err := c.metricsclientset.NodeMetricses().Get(ctx, obj.Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, fmt.Errorf("unable to get metrics for obj=%s metricType=%s metricName=%s: %w", obj, metricType, metricName, err)
+			}
+			cv.NodeMetrics = nodeMetrics
+		case metricNameInletTemp, metricNameDeltaP:
+			metricValue, err := c.custommetricsclient.RootScopedMetrics().GetForObject(schema.GroupKind{Group: "", Kind: "node"}, obj.Name, metricName, labels.NewSelector())
+			if err != nil {
+				return nil, fmt.Errorf("unable to get metrics for obj=%s metricType=%s metricName=%s: %w", obj, metricType, metricName, err)
+			}
+			cv.CustomMetrics[metricName] = metricValue
+		default:
+			return nil, fmt.Errorf("unknown metricName=%s for metricType=%s", metricName, metricType)
+		}
+	case metricTypePod:
+		switch metricName {
+		case metricNameMetrics:
+			podMetrics, err := c.metricsclientset.PodMetricses(obj.Namespace).Get(ctx, obj.Name, metav1.GetOptions{})
+			if err != nil {
+				return nil, fmt.Errorf("unable to get metrics for obj=%s metricType=%s metricName=%s: %w", obj, metricType, metricName, err)
+			}
+			cv.PodMetrics = podMetrics
+		default:
+			return nil, fmt.Errorf("unknown metricName=%s for metricType=%s", metricName, metricType)
+		}
+	default:
+		return nil, fmt.Errorf("unknown metricType=%s", metricType)
+	}
+
+	c.cache.Store(key, cv)
+
+	return cv, nil
+}
+
+func (c *CachedMetricsClient) GetNodeMetrics(ctx context.Context, name string) (*metricsv1beta1.NodeMetrics, error) {
+	cv, err := c.get(ctx, types.NamespacedName{Name: name}, metricTypeNode, metricNameMetrics)
+	if err != nil {
+		return nil, err
+	}
+	return cv.NodeMetrics, nil
+}
+
+func (c *CachedMetricsClient) GetPodMetrics(ctx context.Context, namespace string, name string) (*metricsv1beta1.PodMetrics, error) {
+	cv, err := c.get(ctx, types.NamespacedName{Namespace: namespace, Name: name}, metricTypePod, metricNameMetrics)
+	if err != nil {
+		return nil, err
+	}
+	return cv.PodMetrics, nil
+}
+
+func (c *CachedMetricsClient) GetCustomMetricForNode(ctx context.Context, name string, metricName string) (*custommetricsv1beta2.MetricValue, error) {
+	cv, err := c.get(ctx, types.NamespacedName{Name: name}, metricTypeNode, metricName)
+	if err != nil {
+		return nil, err
+	}
+	return cv.CustomMetrics[metricName], nil
+}
---
+++ wao-core/pkg/client/predictor.go
@@ -0,0 +1,121 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	waov1beta1 "github.com/waok8s/wao-core/api/wao/v1beta1"
+	"github.com/waok8s/wao-core/pkg/predictor"
+	"github.com/waok8s/wao-core/pkg/predictor/fromnodeconfig"
+)
+
+type CachedPredictorClient struct {
+	client client.Client
+
+	ttl   time.Duration
+	cache sync.Map
+}
+
+func NewCachedPredictorClient(client client.Client, ttl time.Duration) *CachedPredictorClient {
+	return &CachedPredictorClient{
+		client: client,
+		ttl:    ttl,
+	}
+}
+
+func predictorCacheKey(valueType string,
+	namespace string, endpointTerm *waov1beta1.EndpointTerm, // common
+	predictorType predictor.PredictorType, // GetPredictorEndpoint
+	cpuUsage, inletTemp, deltaP float64, // PredictPowerConsumption
+) string {
+	ep := fmt.Sprintf("%s|%s|%s", endpointTerm.Type, endpointTerm.Endpoint, endpointTerm.BasicAuthSecret.Name)
+	return fmt.Sprintf("%s#%s#%s#%s#%f#%f#%f", valueType, namespace, ep, predictorType, cpuUsage, inletTemp, deltaP)
+}
+
+const (
+	valueTypePowerConsumptionEndpoint = "PowerConsumptionEndpoint"
+	valueTypeWatt                     = "Watt"
+
+	// valueTypeResponseTimeEndpoint = "ResponseTimeEndpoint"
+	// valueTypeResponseTime         = "ResponseTime"
+)
+
+type predictionCache struct {
+	PowerConsumptionEndpoint *waov1beta1.EndpointTerm
+	Watt                     float64
+
+	// ResponseTimeEndpoint *waov1beta1.EndpointTerm
+	// ResponseTime         float64
+
+	ExpiredAt time.Time
+}
+
+func (c *CachedPredictorClient) do(ctx context.Context, valueType string,
+	namespace string, endpointTerm *waov1beta1.EndpointTerm, // common
+	predictorType predictor.PredictorType, // GetPredictorEndpoint
+	cpuUsage, inletTemp, deltaP float64, // PredictPowerConsumption
+) (*predictionCache, error) {
+
+	key := predictorCacheKey(valueType, namespace, endpointTerm, predictorType, cpuUsage, inletTemp, deltaP)
+
+	if v, ok1 := c.cache.Load(key); ok1 {
+		if cv, ok2 := v.(*predictionCache); ok2 {
+			if cv.ExpiredAt.After(time.Now()) {
+				return cv, nil
+			}
+		}
+	}
+
+	cv := &predictionCache{
+		ExpiredAt: time.Now().Add(c.ttl),
+	}
+
+	switch valueType {
+	case valueTypePowerConsumptionEndpoint:
+		prov, err := fromnodeconfig.NewEndpointProvider(c.client, namespace, endpointTerm)
+		if err != nil {
+			return nil, err
+		}
+		ep, err := prov.Get(ctx, predictorType)
+		if err != nil {
+			return nil, err
+		}
+		cv.PowerConsumptionEndpoint = ep
+	case valueTypeWatt:
+		pred, err := fromnodeconfig.NewPowerConsumptionPredictor(c.client, namespace, endpointTerm)
+		if err != nil {
+			return nil, err
+		}
+		watt, err := pred.Predict(ctx, cpuUsage, inletTemp, deltaP)
+		if err != nil {
+			return nil, err
+		}
+		cv.Watt = watt
+	default:
+		return nil, fmt.Errorf("unknown valueType=%s", valueType)
+	}
+
+	c.cache.Store(key, cv)
+
+	return cv, nil
+}
+
+func (c *CachedPredictorClient) GetPredictorEndpoint(ctx context.Context, namespace string, ep *waov1beta1.EndpointTerm, predictorType predictor.PredictorType) (*waov1beta1.EndpointTerm, error) {
+	cv, err := c.do(ctx, valueTypePowerConsumptionEndpoint, namespace, ep, predictorType, 0.0, 0.0, 0.0)
+	if err != nil {
+		return nil, err
+	}
+	return cv.PowerConsumptionEndpoint, nil
+}
+
+func (c *CachedPredictorClient) PredictPowerConsumption(ctx context.Context, namespace string, ep *waov1beta1.EndpointTerm, cpuUsage, inletTemp, deltaP float64) (watt float64, err error) {
+	cv, err := c.do(ctx, valueTypeWatt, namespace, ep, "", cpuUsage, inletTemp, deltaP)
+	if err != nil {
+		return 0.0, err
+	}
+	return cv.Watt, nil
+}
