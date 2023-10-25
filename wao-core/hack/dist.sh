2e2903669c744c2d7e7440e41b6bdb39d684b4a8
Shunsuke Ise <ise@bitmedia.co.jp>
2023-10-25 16:12:19 +0900

    CI

--- wao-core/hack/dist.sh
+++ wao-core/hack/dist.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-VERSION=$(git describe --tags --match "v*")
+# consts
 IMAGE=wao-controller
+DIST=wao-core.yaml
+
+VERSION=$(git describe --tags --match "v*")
 REGISTRY=${REGISTRY:-""}
+IMAGE_NAME=$IMAGE:$VERSION
+if [ -n "$REGISTRY" ] ; then IMAGE_NAME=$REGISTRY/$IMAGE:$VERSION ; fi
 
-IMAGE_NAME=$BIN:$VERSION
-if [ -n "$REGISTRY" ]; then
-    IMAGE_NAME=$REGISTRY/$IMAGE:$VERSION
-fi
-DIST=wao-core-$VERSION.yaml
-
+# main
 make kustomize
-
 cd config/manager && "../../bin/kustomize" edit set image controller="$IMAGE_NAME" && cd -
 "./bin/kustomize" build config/default > $DIST
