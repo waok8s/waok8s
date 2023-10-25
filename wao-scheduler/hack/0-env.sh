53915090cc4193e5586169e652d21bf13a3b79d9
Shunsuke Ise <ise@bitmedia.co.jp>
2023-10-25 16:27:33 +0900

    CI

--- wao-scheduler/hack/0-env.sh
+++ wao-scheduler/hack/0-env.sh
@@ -9,6 +9,7 @@ PROJECT_ROOT=$(dirname "$(dirname "$SCRIPT")")
 PROJECT_NAME=$(basename "$PROJECT_ROOT")
 
 KUBECTL_VERSION=v1.27.3
+KUSTOMIZE_VERSION=v5.2.1
 KIND_VERSION=v0.20.0
 
 LOCALBIN=$PROJECT_ROOT/bin # bin/
@@ -16,5 +17,6 @@ LOCALBIN=$PROJECT_ROOT/bin # bin/
 KIND=$LOCALBIN/kind # bin/kind
 KUBECTL_DIR=$LOCALBIN/kubectl-$KUBECTL_VERSION # bin/kubectl-v1.27.3
 KUBECTL=$KUBECTL_DIR/kubectl # bin/kubectl-v1.27.3/kubectl
+KUSTOMIZE=$LOCALBIN/kustomize # bin/kustomize
 
 DOCKER=$(which docker) # assumes docker command is available
--- wao-scheduler/hack/1-bin.sh
+++ wao-scheduler/hack/1-bin.sh
@@ -7,12 +7,14 @@
 
 test -s "$KIND" || GOBIN="$LOCALBIN" go install sigs.k8s.io/kind@"$KIND_VERSION"
 test -s "$KUBECTL" || (mkdir -p "$KUBECTL_DIR" ; curl -L https://dl.k8s.io/release/"$KUBECTL_VERSION"/bin/linux/amd64/kubectl > "$KUBECTL" ; chmod +x "$KUBECTL")
+test -s "$KUSTOMIZE" || (curl -s -L https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F"$KUSTOMIZE_VERSION"/kustomize_"$KUSTOMIZE_VERSION"_linux_amd64.tar.gz | tar xvzf - -C "$LOCALBIN")
 test -s "$DOCKER" || (echo "[ERROR] command not found: docker" && exit 1)
 
 echo -e "= version info =\n"
 
 "$KIND" version
 "$KUBECTL" version -oyaml
+"$KUSTOMIZE" version -oyaml
 "$DOCKER" version
 
 echo -e "================\n"
