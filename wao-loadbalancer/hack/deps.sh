005e4fb20400d29d765ed8edb0e75006130db823
Shunsuke Ise <ise@bitmedia.co.jp>
2025-02-20 17:51:33 +0900

    avoid config conflictions

--- wao-loadbalancer/hack/deps.sh
+++ wao-loadbalancer/hack/deps.sh
@@ -2,6 +2,8 @@
 
 WAO_CORE_VER="v1.30.0"
 WAO_METRICS_ADAPTER_VER="v1.30.0"
+WAO_SCHEDULER_VER="v1.30.0"
 
 cd config/base/deps && curl -LO "https://github.com/waok8s/wao-core/releases/download/$WAO_CORE_VER/wao-core.yaml" && cd -
 cd config/base/deps && curl -LO "https://github.com/waok8s/wao-metrics-adapter/releases/download/$WAO_METRICS_ADAPTER_VER/wao-metrics-adapter.yaml" && cd -
+cd config/base/deps && curl -LO "https://github.com/waok8s/wao-scheduler/releases/download/$WAO_SCHEDULER_VER/wao-scheduler.yaml" && cd -
