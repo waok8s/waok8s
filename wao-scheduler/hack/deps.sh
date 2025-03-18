62159e207dacdc2f8b522a4408f0327660e1ce11
Shunsuke Ise <ise@bitmedia.co.jp>
2025-03-18 21:53:32 +0900

    scheduler: change domain to `waok8s.github.io`

--- wao-scheduler/hack/deps.sh
+++ wao-scheduler/hack/deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WAO_CORE_VER="v1.30.1"
-WAO_METRICS_ADAPTER_VER="v1.30.1"
+WAO_CORE_VER="v1.30.3-alpha.0"
+WAO_METRICS_ADAPTER_VER="v1.30.3-alpha.0"
 
 cd config/base/deps && curl -LO "https://github.com/waok8s/waok8s/releases/download/wao-core/$WAO_CORE_VER/wao-core.yaml" && cd -
 cd config/base/deps && curl -LO "https://github.com/waok8s/waok8s/releases/download/wao-metrics-adapter/$WAO_METRICS_ADAPTER_VER/wao-metrics-adapter.yaml" && cd -
