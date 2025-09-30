e7e2a55ac5692af37f55c7f905dd6386dfa598d9
Shunsuke Ise <ise@bitmedia.co.jp>
2025-09-30 18:40:50 +0900

    wao-scheduler/v1.31.0

--- wao-scheduler/hack/deps.sh
+++ wao-scheduler/hack/deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-WAO_CORE_VER="v1.31.0-beta.0"
-WAO_METRICS_ADAPTER_VER="v1.31.0-beta.0"
+WAO_CORE_VER="v1.31.0"
+WAO_METRICS_ADAPTER_VER="v1.31.0"
 
 cd config/base/deps && curl -LO "https://github.com/waok8s/waok8s/releases/download/wao-core/$WAO_CORE_VER/wao-core.yaml" && cd -
 cd config/base/deps && curl -LO "https://github.com/waok8s/waok8s/releases/download/wao-metrics-adapter/$WAO_METRICS_ADAPTER_VER/wao-metrics-adapter.yaml" && cd -
